6e9a4fb84eb580382954f5a9f1f76f77adddbaa6
Chad Linthicum <portableanswers@gmail.com>
2023-12-16 19:03:56 -0500

    Updated Unity version. Nunchuk working.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 3f;
+
+    private InputAction touchClickAction;
+
+    private void Start()
+    {
+        // Find the input action for touch click on the Oculus Go controller touchpad
+        touchClickAction = new InputAction("OculusGo/TouchClick", InputActionType.Button, "<XRController>{RightHand}/primaryButton");
+        touchClickAction.Enable();
+        touchClickAction.performed += OnTouchClickPerformed;
+    }
+
+    private void Update()
+    {
+        // ...
+    }
+
+    private void OnTouchClickPerformed(InputAction.CallbackContext context)
+    {
+        MoveForward();
+    }
+
+    private void MoveForward()
+    {
+        Vector3 forward = transform.forward;
+        forward.y = 0f; // Remove vertical component for movement on the XZ plane
+        transform.position += forward.normalized * speed * Time.deltaTime;
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# GAME-Armored_Crusade_VR
-The Armored Crusade you know and love but it full force VR!!
